ea8b36a157f662656181e37dc87a0df381aa7dae
HKEDU <HKEDU@DESKTOP-9CVNMAM>
2023-11-20 13:45:00 +0900

    ㅁㅁ

---
+++ whitetoon/src/test/java/com/toon/WhitetoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitetoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
